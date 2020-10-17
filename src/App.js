0c0376d6088750e2064d269b55ce69519f6241c9
Thiago Abad <abadthiago@gmail.com>
2020-10-17 18:08:32 +0100

    Fix: Linted

--- src/App.js
+++ src/App.js
@@ -1,53 +1,53 @@
 import React from 'react';
 import './App.css';
 import 'fontsource-roboto';
-import { Container, Typography } from "@material-ui/core";
+import { Container, Typography } from '@material-ui/core';
 
 import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
 
-const cpfsInvalidos = ["00000000000", "11111111111", "22222222222", "33333333333", "44444444444", "55555555555", "66666666666", "77777777777", "88888888888", "99999999999"]
+const cpfsInvalidos = ['00000000000', '11111111111', '22222222222', '33333333333', '44444444444', '55555555555', '66666666666', '77777777777', '88888888888', '99999999999'];
 
 function App() {
-  return (
-    <Container component="article" maxWidth="sm">
-      <Typography
-        variant="h3"
-        component="h1"
-        align="center">
+    return (
+        <Container component="article" maxWidth="sm">
+            <Typography
+                variant="h3"
+                component="h1"
+                align="center">
         Formulário de cadastro
-      </Typography>
-      <FormularioCadastro aoEnviar={aoEnviarForm} validarCpf={validarCpf} />
-    </Container>
-  );
+            </Typography>
+            <FormularioCadastro aoEnviar={aoEnviarForm} validarCpf={validarCpf} />
+        </Container>
+    );
 }
 
 function aoEnviarForm(dados) {
-  console.log(dados);
+    console.log(dados);
 }
 
 function validarCpf(cpf) {
-  if (cpf.length !== 11) {
-    return { cpf: { invalido: true, texto: "CPF deve ter 11 dígitos" } };
-  } else {
-      var Soma;
-      var Resto;
-      Soma = 0;
-      if (cpfsInvalidos.includes(cpf)) return { cpf: { invalido: true, texto: "CPF inválido" } };
+    if (cpf.length !== 11) {
+        return { cpf: { invalido: true, texto: 'CPF deve ter 11 dígitos' } };
+    } else {
+        var Soma;
+        var Resto;
+        Soma = 0;
+        if (cpfsInvalidos.includes(cpf)) return { cpf: { invalido: true, texto: 'CPF inválido' } };
 
-      for (let i = 1; i <= 9; i++) Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
-      Resto = (Soma * 10) % 11;
+        for (let i = 1; i <= 9; i++) Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+        Resto = (Soma * 10) % 11;
 
-      if ((Resto === 10) || (Resto === 11)) Resto = 0;
-      if (Resto !== parseInt(cpf.substring(9, 10))) return { cpf: { invalido: true, texto: "CPF inválido" } };
+        if ((Resto === 10) || (Resto === 11)) Resto = 0;
+        if (Resto !== parseInt(cpf.substring(9, 10))) return { cpf: { invalido: true, texto: 'CPF inválido' } };
 
-      Soma = 0;
-      for (let i = 1; i <= 10; i++) Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
-      Resto = (Soma * 10) % 11;
+        Soma = 0;
+        for (let i = 1; i <= 10; i++) Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+        Resto = (Soma * 10) % 11;
 
-      if ((Resto === 10) || (Resto === 11)) Resto = 0;
-      if (Resto !== parseInt(cpf.substring(10, 11))) return { cpf: { invalido: true, texto: "CPF inválido" } };
-      return { cpf: { invalido: false, texto: "" } };
-  }
+        if ((Resto === 10) || (Resto === 11)) Resto = 0;
+        if (Resto !== parseInt(cpf.substring(10, 11))) return { cpf: { invalido: true, texto: 'CPF inválido' } };
+        return { cpf: { invalido: false, texto: '' } };
+    }
 
 }
 
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,19 +1,25 @@
 import React, { useState } from 'react';
-import { Button, TextField, FormControlLabel, Switch } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import {
+    Button, TextField, FormControlLabel, Switch,
+} from '@material-ui/core';
 
-function FormularioCadastro({aoEnviar, validarCpf}) {
-    const [nome, setNome] = useState("");
-    const [sobrenome, setSobrenome] = useState("");
-    const [cpf, setCpf] = useState("");
+function FormularioCadastro({ aoEnviar, validarCpf }) {
+    const [nome, setNome] = useState('');
+    const [sobrenome, setSobrenome] = useState('');
+    const [cpf, setCpf] = useState('');
     const [promocoes, setPromocoes] = useState(true);
     const [novidades, setNovidades] = useState(true);
-    const [erros, setErros] = useState({cpf: {invalido:false, texto:""}});
+    const [erros, setErros] = useState({ cpf: { invalido: false, texto: '' } });
 
     return (
-        <form onSubmit={event => {
+        <form onSubmit={(event) => {
             event.preventDefault();
-            aoEnviar({nome, sobrenome, cpf, novidades, promocoes});
-        }}>
+            aoEnviar({
+                nome, sobrenome, cpf, novidades, promocoes,
+            });
+        }}
+        >
             <TextField
                 id="nome"
                 label="Nome"
@@ -21,7 +27,7 @@ function FormularioCadastro({aoEnviar, validarCpf}) {
                 variant="outlined"
                 margin="normal"
                 fullWidth
-                onChange={event => { setNome(event.target.value) }}
+                onChange={(event) => { setNome(event.target.value); }}
             />
             <TextField
                 id="sobrenome"
@@ -30,7 +36,7 @@ function FormularioCadastro({aoEnviar, validarCpf}) {
                 variant="outlined"
                 margin="normal"
                 fullWidth
-                onChange={event => { setSobrenome(event.target.value) }}
+                onChange={(event) => { setSobrenome(event.target.value); }}
             />
             <TextField
                 id="cpf"
@@ -39,41 +45,45 @@ function FormularioCadastro({aoEnviar, validarCpf}) {
                 variant="outlined"
                 margin="normal"
                 fullWidth
-                onChange={event => { setCpf(event.target.value) }}
-                onBlur={event => { 
-                    setErros(validarCpf(event.target.value))
-                 }}
+                onChange={(event) => { setCpf(event.target.value); }}
+                onBlur={(event) => {
+                    setErros(validarCpf(event.target.value));
+                }}
                 error={erros.cpf.invalido}
                 helperText={erros.cpf.texto}
             />
 
             <FormControlLabel
-                control={
+                control={(
                     <Switch
                         checked={promocoes}
                         name="promocoes"
                         color="primary"
-                        onChange={event => { setPromocoes(event.target.checked) }}
+                        onChange={(event) => { setPromocoes(event.target.checked); }}
                     />
-                }
+                )}
                 label="Promoções"
             />
             <FormControlLabel
-                control={
+                control={(
                     <Switch
                         checked={novidades}
                         name="novidades"
                         color="primary"
-                        onChange={event => { setNovidades(event.target.checked) }}
+                        onChange={(event) => { setNovidades(event.target.checked); }}
                     />
-                }
+                )}
                 label="Novidades"
             />
 
             <Button color="primary" variant="contained" type="submit" disabled={erros.cpf.invalido}>Cadastrar</Button>
         </form>
-    )
-
+    );
 }
+
+FormularioCadastro.propTypes = {
+    aoEnviar: PropTypes.func.isRequired,
+    validarCpf: PropTypes.object.isRequired,
+};
 
 export default FormularioCadastro;
